baafa5e323960d8ccc34ed024a71f192f7de7104
Aleksandar A. <aleksandaratanasovski1994@gmail.com>
2024-06-21 08:41:07 +0200

    updated packages

--- pages/index.vue
+++ pages/index.vue
@@ -36,7 +36,7 @@ if (import.meta.client) {
         :showReset="true" />
       <div
         class="grid grid-cols-1 gap-8 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6"
-        v-if="!!allListings">
+        v-if="allListings && allListings.length > 0">
         <ListingCard
           v-for="listing in allListings"
           :listing
